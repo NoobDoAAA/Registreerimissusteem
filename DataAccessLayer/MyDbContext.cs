15883e2f2ba05cd5d4826b611abd292d5bd441e7
Alex Gavrilov <noobdoaaa@gmail.com>
2023-11-19 08:17:49 +0200

    + EF Konfigureerimine

---
+++ DataAccessLayer/MyDbContext.cs
@@ -0,0 +1,12 @@
+﻿using DataAccessLayer.DbModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Uritus> Uritused { get; set; }
+    }
+}
